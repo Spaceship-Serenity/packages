ca4bb3ba47a2a59fb1f19ec73fe5dd0a14217eb4
Alek <aleksander.zywot@mountsinai.org>
2020-06-23 07:01:02 -0400

    add py2.7 to manylinux docker

--- manylinux/build/build.sh
+++ manylinux/build/build.sh
@@ -2,7 +2,8 @@
 set -e -u -x
 
 
-pip="/opt/python/cp36-cp36m/bin/pip"
+# pip="/opt/python/cp36-cp36m/bin/pip"
+pip="/opt/python/cp27-cp27m/bin/pip"
 
 echo "$pip"
 
@@ -13,16 +14,3 @@ for pkg in /build/src/*; do
     "$pip" install "$pkg"
     "$pip" wheel "$pkg" --no-deps -w /build/stage/
 done
-
-
-# Bundle external shared libraries into the wheels
-echo "aduiting dependencies"
-for whl in /build/stage/*.whl; do
-    if ! auditwheel show "$whl"; then
-        echo "Skipping non-platform wheel $whl"
-        mv "$whl" /build/output/
-    else
-        auditwheel repair  "$whl" -w /build/output/
-    fi
-done
-
